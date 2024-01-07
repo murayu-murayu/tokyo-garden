624a24a996b53354db8212ea8dd006dc2935c776
Murata Yu <muratayu@team-lab.com>
2024-01-07 17:16:13 +0900

    [update] top page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,16 @@
-import type { Config } from 'tailwindcss'
+import { Config } from "tailwindcss";
 
-const config: Config = {
+const tailwindConfig: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/app/**/*.{ts,tsx}",
+    "./src/components/**/*.{ts,tsx}",
+    "./content/**/*.{md,mdx}",
   ],
+  darkMode: "class",
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    extend: {},
   },
-  plugins: [],
-}
-export default config
+  plugins: [require("@tailwindcss/typography")],
+};
+
+export default tailwindConfig;
